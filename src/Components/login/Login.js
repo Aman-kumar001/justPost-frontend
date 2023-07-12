fac54752e785beaeb8a235403c01a6c07e16a6a9
Aman Kumar <amankum@maqsoftware.com>
2023-07-12 13:50:15 +0530

    started

---
+++ src/Components/login/Login.js
@@ -0,0 +1,10 @@
+import styles from './login.module.css';
+const Login = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.login}>asdf</div>
+		</div>
+	);
+};
+
+export default Login;
