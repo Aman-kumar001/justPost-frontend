8a13f81979748cb6024de44f55d553c2f24a6e57
Aman Kumar <2019200@iiitdmj.ac.in>
2023-07-14 00:04:32 +0530

    dash

--- src/Components/dashboard/Dashboard.js
+++ src/Components/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
-import { Avatar, Menu, TextField } from '@mui/material';
+import { Avatar, Menu, TextField, Button } from '@mui/material';
 import styles from './dashboard.module.css';
 import Navbar from '../Navbar/Navbar';
-import { Button, TextareaAutosize } from '@mui/base';
+
 const Dashboard = () => {
 	return (
 		<div className={styles.container}>
@@ -10,14 +10,19 @@ const Dashboard = () => {
 				<div>a</div>
 				<div className={styles.contentSpace}>
 					<form action='' className={styles.postForm}>
-						<TextareaAutosize minRows={2} />
+						<TextField />
 						<div>
 							<Button variant='contained'>Post</Button>
 						</div>
 						<hr />
 					</form>
 				</div>
-				<div>a</div>
+				<div className={styles.user}>
+					<div className={styles.AvatarCont}>
+						<Avatar className={styles.avatar} />
+					</div>
+					<div className={styles.userDetails}>Aman</div>
+				</div>
 			</div>
 		</div>
 	);
