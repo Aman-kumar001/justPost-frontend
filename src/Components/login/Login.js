cdfa40f7c59c3c6ca6e77312c84d7521efe6efb9
Aman Kumar <amankum@maqsoftware.com>
2023-07-18 10:46:09 +0530

    login issue fixed

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -35,20 +35,24 @@ const Login = ({ token, setToken }) => {
 				if (res.status == 200) {
 					console.log('loged in');
 					res.json().then((data) => {
-						console.log(data);
-						setToken({
-							id: data.data[0]._id,
-							name: data.data[0].name,
-						});
-						localStorage.setItem(
-							'token',
-							JSON.stringify({
+						if (data.data.length > 0) {
+							console.log(data);
+							setToken({
 								id: data.data[0]._id,
 								name: data.data[0].name,
-							})
-						);
+							});
+							localStorage.setItem(
+								'token',
+								JSON.stringify({
+									id: data.data[0]._id,
+									name: data.data[0].name,
+								})
+							);
+							navigate('/dashboard');
+						} else {
+							alert('Wrong username or password');
+						}
 					});
-					navigate('/dashboard');
 				} else {
 					setError(true);
 					setTimeout(() => {
